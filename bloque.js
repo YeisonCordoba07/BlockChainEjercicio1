5cd98ab6095badcd109c5cddede2b100f08d67d5
YEISON ALEXANDER CORDOBA MENA <41711172+YeisonCordoba07@users.noreply.github.com>
2024-02-15 17:44:40 +0000

    agregar setters y getters

--- bloque.js
+++ bloque.js
@@ -9,9 +9,53 @@ class Boque {
     }
 
     crearHash() {
-        let nuevoHash = calcularSHA256(this.bloque, this.nonce, this.datos, this.hashAnterior);
+        let nuevoHash = calcularSHA256(this.bloque.toString() + this.nonce.toString() + 
+        this.datos + this.hashAnterior);
 
         this.hash = nuevoHash;
     }
+
+    //-------------------------------------------
+    set setBloque(nuevoBloque){
+        this.bloque = nuevoBloque.toString();
+        crearHash();
+    }
+
+    set setNonce(nuevoBNonce){
+        this.nonce = nuevoNonce.toString();
+        crearHash();
+    }
+
+    set setDatos(nuevosDatos){
+        this.datos = nuevosDatos;
+        crearHash();
+    }
+    set setHashAnterior(nuevoHashAnterior){
+        this.bloque = nuevoHashAnterior;
+        crearHash();
+    }
+
+    //-------------------------------------------
+    get getBloque() {
+        return this.bloque;
+    }
+
+    get getNonce() {
+        return this.nonce;
+    }
+
+    get getDatos() {
+        return this.datos;
+    }
+
+    get getHashAnterior() {
+        return this.hashAnterior;
+    }
+
+    get getHash() {
+        return this.hash;
+    }
+
+    
 
 }
--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import { calcularSHA256 } from './sha256.js';
+import bloque from "./bloque.js";
 
 
-
-
+const bloque1 = new bloque(1, 0, "", "", "");
+const bloque2 = new bloque(2, 0, "", "", "");
+const bloque3 = new bloque(3, 0, "", "", "");
+const bloque4 = new bloque(4, 0, "", "", "");
+const bloque5 = new bloque(5, 0, "", "", "");
+const bloque6 = new bloque(6, 0, "", "", "");
 
 // Ejemplo de uso
 const mensaje = "hola";
